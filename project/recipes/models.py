b17391e0a6a5d71def5c888b681a493fd1062d76
notKguin <kvitonovmaksim1@gmail.com>
2025-11-07 10:58:08 +0500

    konets

--- project/recipes/models.py
+++ project/recipes/models.py
@@ -5,6 +5,7 @@ class Recipe(models.Model):
     description = models.TextField('Описание', blank=True)
     ingredients = models.TextField('Ингредиенты', blank=True)
     steps = models.TextField('Шаги', blank=True)
+    colories = models.BooleanField(("Диетическое"))
 
     def __str__(self):
         return self.title
